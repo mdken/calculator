1328a23825d99a65ef8112e9b784a2654e0d65c2
Michael D. Kennedy <mdkennedy1@gmail.com>
2016-02-24 16:28:14 -0500

    completed

--- calc.js
+++ calc.js
@@ -1,15 +1,60 @@
-var add = function(num1, num2) {
-	return (num1 + num2);
+function add(a, b){
+    return a + b;
 };
 
-var multiply = function(num1, num2) {
-	return (num1 * num2);
+function multiply(a, b){
+    return a * b;
 };
 
-var subtract = function(num1, num2) {
-	return (num1 - num2);
+function divide(a, b){
+    return a / b;
 };
 
-var divide = function(num1, num2) {
-	return (num1 / num2);
+function subtract(a, b){
+    return (a - b);
 };
+
+$(function(){
+    var current = "0";
+    var first = 0;
+    var op = null;
+    
+    $('[class^=num], [class^=calc]').click(function(){
+        var button = $(this).attr("class").split("_");
+        var type = button[0];
+        button = button[button.length - 1];
+        if (type == "num"){
+            if (current == "0"){
+                current = button;
+            } else {
+                current += button;
+            }
+        } else {
+            if (button == "equal"){
+                switch(op) {
+                    case "add":
+                        current = String(add(Number(current), first));
+                        break;
+                    case "subtract":
+                        current = String(subtract(first,  Number(current)));
+                        break;
+                    case "multiply":
+                        current = String(multiply(Number(current), first));
+                        break;
+                    case "divide":
+                        current = String(divide(first, Number(current)));
+                        break;
+                }
+            } else if(button == "clear") {
+                current = "0";
+                first = 0;
+                op = null;
+            } else {
+                first = Number(current);
+                op = button;
+                current = "0";
+            }
+        }
+        $('#result').html(current);
+    });
+});
